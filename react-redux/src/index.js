4cd2c770f5f62d4e1cde20071438a4f173492755
Cuckoo-sir <1339383252@qq.com>
2018-04-13 17:43:01 +0800

    redux
    redux

--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -1,22 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Header from './Header';
-import Content from './Content';
-import { Provider  } from './react-redux'
+import { createStore } from 'redux';
+import { Provider  } from 'react-redux';
+import Header from './containers/Header'
+import Content from './containers/Content'
 import './index.css';
-
-function createStore(reducer) {
-    let state = null;
-    const listeners = [];
-    const subscribe = (listener) => listeners.push(listener);
-    const getState = () => state;
-    const dispatch = (action) => {
-        state = reducer(state, action);
-        listeners.forEach((listener) => listener())
-    }
-    dispatch({}) // 初始化state
-    return {getState, dispatch, subscribe}
-}
 
 const themeReducer = (state, action) => {
     if (!state) return {
@@ -31,6 +19,19 @@ const themeReducer = (state, action) => {
 }
 
 const store = createStore(themeReducer)
+
+// function createStore(reducer) {
+//     let state = null;
+//     const listeners = [];
+//     const subscribe = (listener) => listeners.push(listener);
+//     const getState = () => state;
+//     const dispatch = (action) => {
+//         state = reducer(state, action);
+//         listeners.forEach((listener) => listener())
+//     }
+//     dispatch({}) // 初始化state
+//     return {getState, dispatch, subscribe}
+// }
 
 
 class Index extends Component {
